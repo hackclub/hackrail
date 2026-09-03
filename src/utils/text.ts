f09c7f23b8cea9b8da43fada8e28afabded2244c
alice <122629939+espcaa@users.noreply.github.com>
2026-09-03 21:06:07 +0200

    Added & updated a bunch of stuff

---
+++ src/utils/text.ts
@@ -0,0 +1,31 @@
+const breakList = ["$"];
+
+export type TextBlock = {
+  text: string;
+  isInBreakList: boolean;
+};
+
+function separateText(text: string): TextBlock[] {
+  const result: TextBlock[] = [];
+  let currentText = "";
+
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+
+    if (breakList.includes(char)) {
+      result.push({ text: currentText, isInBreakList: false });
+      result.push({ text: char, isInBreakList: true });
+      currentText = "";
+    } else {
+      currentText += char;
+    }
+  }
+
+  if (currentText) {
+    result.push({ text: currentText, isInBreakList: false });
+  }
+
+  return result;
+}
+
+export { separateText };
